5121cf2b664a129d9b06c16ee1036fbeb6cf6e6e
avneeshchaudhary4 <avneeshpatel20842@gmail.com>
2023-08-27 01:28:25 +0530

    first commit

---
+++ Assignment-4.js
@@ -0,0 +1,7 @@
+function hasProperty(obj, propertyName) {
+    return obj.hasOwnProperty(propertyName);
+  }
+  
+  const exampleObject = { name: "Alice", age: 25 };
+  console.log(hasProperty(exampleObject, "age")); 
+  console.log(hasProperty(exampleObject, "email"));
